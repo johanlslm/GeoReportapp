6ae9f3e8b16de2fda82c1075e999cf864198352a
johan.lozano.g@gmail.com <johan.lozano.g@gmail.com>
2022-09-24 12:15:19 -0500

    conf gps

---
+++ AppGeolocalizacionHuecos/Scripts/Modulos/SearchGPS.js
@@ -0,0 +1,14 @@
+﻿function onMapClick(e) {
+	var cordinateMouse = L.marker(e.latlng);
+	cordinateMouse.addTo(mymap);
+	var cordinate2 = e.latlng.toString();
+	alert(cordinate2);
+	var array = cordinate2.split(',');
+	var b = array[0];
+	b = b.replace(/[^\d.-]/g, '');
+	b = parseFloat(b);
+	var c = array[1];
+	c = c.replace(/[^\d.-]/g, '');
+	c = parseFloat(c);
+}
+alert("entro")
